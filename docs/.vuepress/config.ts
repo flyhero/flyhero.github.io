12d79b3d6fce806be99bfac70e55a18529a81fcc
flyhero <qfwang666@163.com>
2023-08-19 17:56:35 +0800

    feat: 禁用贡献者

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -81,7 +81,7 @@ export default defineUserConfig({
         editLink: false,
         editLinkText: '在 GitHub 上编辑此页',
         lastUpdatedText: '上次更新',
-        contributorsText: '贡献者',
+        // contributorsText: '贡献者',
         // custom containers
         tip: '提示',
         warning: '注意',
